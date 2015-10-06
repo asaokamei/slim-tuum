bb37985689b3b48af71d2a5187655c9f4a68893e
Asao <kamei@workspot.jp>
2015-10-06 20:13:18 +0900

    removed controller related code.
    Tuum\Respond no longer contains controller code.

--- app/routes.php
+++ app/routes.php
@@ -2,10 +2,9 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Http\UploadedFile;
 use Tuum\Respond\Respond;
 use Tuum\Slimmed\DocumentMap;
-
-require __DIR__ . '/classes/UploadController.php';
 
 /**
  * routes
@@ -55,7 +54,34 @@ $app->get('/throw', function() {
 /**
  * file upload example
  */
-$app->any('/upload', UploadController::class);
+$app->get('/upload', function(Request $request, Response $response) {
+    return Respond::view($request, $response)
+        ->withReqAttribute('csrf_name', 'csrf_value')
+        ->asView('upload');
+});
+
+$app->post('/upload', function(Request $request, Response $response) {
+    $responder = Respond::redirect($request, $response);
+    $uploaded  = $request->getUploadedFiles();
+    $responder
+        ->with('isUploaded', true)
+        ->with('dump', print_r($uploaded, true));
+    /** @var UploadedFile $upload */
+    $upload = $uploaded['up'][0];
+    $responder->with('upload', $upload);
+
+    if ($upload->getError()===UPLOAD_ERR_NO_FILE) {
+        $responder->withErrorMsg('please uploaded a file');
+    } elseif ($upload->getError()===UPLOAD_ERR_FORM_SIZE || $upload->getError()===UPLOAD_ERR_INI_SIZE) {
+        $responder->withErrorMsg('uploaded file size too large!');
+    } elseif ($upload->getError()!==UPLOAD_ERR_OK) {
+        $responder->withErrorMsg('uploading failed!');
+    } else {
+        $responder->withMessage('uploaded a file');
+    }
+    return $responder
+        ->toPath('/upload');
+});
 
 /**
  * FileMap for Document files

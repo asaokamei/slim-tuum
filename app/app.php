d0321fa81ebda39c539c0d62ca3d727259e5aa94
Asao <kamei@workspot.jp>
2015-11-04 16:19:30 +0900

    renamed method name.

--- app/app.php
+++ app/app.php
@@ -12,12 +12,12 @@ $builder = new AppBuilder(__DIR__.'/config', dirname(__DIR__).'/var');
  * configure with config/builder.php
  */
 $builder->set('twig-dir', __DIR__.'/Demo/twigs');
-$builder->configure('builder');
+$builder->execConfig('builder');
 
 /**
  * import routes
  */
-$builder->evaluate(__DIR__.'/Demo/routes');
+$builder->execute(__DIR__.'/Demo/routes');
 
 /**
  * done construction.

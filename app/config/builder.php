0bc68ea1498774c4f942a0ecf3b9a8f151c41c49
Asao <kamei@workspot.jp>
2015-11-07 11:46:41 +0900

    apply to the next Tuum/Respond structure.

--- app/config/builder.php
+++ app/config/builder.php
@@ -1,29 +1,17 @@
 <?php
 
-use Psr\Http\Message\StreamInterface;
 use Slim\Container;
 use Slim\Csrf\Guard;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Tuum\Respond\Respond;
 use Tuum\Respond\Responder;
-use Tuum\Respond\ResponseHelper;
 use Tuum\Respond\Service\ErrorView;
-use Tuum\Respond\Service\TwigStream;
+use Tuum\Respond\Service\TwigViewer;
 use Tuum\Slimmed\CallableResolver;
 use Tuum\Slimmed\TuumStack;
 
 $container = $app->getContainer();
-
-/**
- * resolving found path to a resolver. 
- * 
- * @param Container $c
- * @return CallableResolver
- */
-$container['callableResolver'] = function($c) {
-    return new CallableResolver($c);
-};
 
 /**
  * C.S.R.F. guardian by Slim. 
@@ -41,39 +29,17 @@ $container['csrf'] = function() {
 $app->add($container['csrf']);
 
 /**
- * set up a response builder. 
- * 
- * @param StreamInterface $stream
- * @param int             $status
- * @param array           $header
- * @return Response
- */
-ResponseHelper::$responseBuilder = function(StreamInterface $stream, $status, array $header) {
-    $response = new Response();
-    $response = $response
-        ->withBody($stream)
-        ->withStatus($status)
-    ;
-    foreach($header as $key => $val) {
-        /** @noinspection PhpUndefinedMethodInspection  ...but why? */
-        $response = $response->withHeader($key, $val);
-    }
-    return $response;
-};
-
-/**
  * use Tuum/Responder with Twig as renderer.
  */
 $container[Responder::class] = function() use($builder) {
 
-    $stream = TwigStream::forge($builder->get('twig-dir'), []);
+    $stream = TwigViewer::forge($builder->get('twig-dir'), []);
     $errors = ErrorView::forge($stream, [
         'default' => 'errors/error',
         'status'  => [
             '404' => 'errors/notFound',
             '403' => 'errors/forbidden',
         ],
-        'handler' => false,
     ]);
     $responder = Responder::build($stream, $errors, 'layouts/contents');
 

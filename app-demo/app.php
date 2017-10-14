ae0b33a6a421b0e73ebb072b7f465d7e8703ccfe
Asao <kamei@workspot.jp>
2017-10-14 17:25:46 +0900

    use setApp() to set application.

--- app-demo/app.php
+++ app-demo/app.php
@@ -1,10 +1,11 @@
 <?php
 use Tuum\Builder\Builder;
 
-$builder = Builder::forge(__DIR__, dirname(__DIR__) . '/var', true);
+$builder = Builder::forge(__DIR__, dirname(__DIR__) . '/var', false);
+$builder->loadEnv();
 $settings = $builder->load('settings');
 $app = new \Slim\App($settings);
-$builder->set('app', $app);
+$builder->setApp($app);
 
 $builder->load('dependencies');
 $builder->load('middleware');
